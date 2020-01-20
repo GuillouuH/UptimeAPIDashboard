c018fcfae3b6407b33ba3340efe5bed98ac63abf
Hugo Guillouet <guillouethugo@gmail.com>
2020-01-20 09:28:05 +0100

    Modif middleware authentication pour vérifier si l’utilisateur existe toujours

--- uptime-api/src/middlewares/authentication.ts
+++ uptime-api/src/middlewares/authentication.ts
@@ -1,19 +1,27 @@
 import  * as jwt from 'jsonwebtoken';
 import * as dotenv from "dotenv";
+import * as mongoose from 'mongoose';
+import { UserSchema, IUser } from '../models/UserModel';
 
 dotenv.config();
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const User = mongoose.model<IUser>('Users', UserSchema);
 
-export function AuthenticateUser(req:any, res:any, next:any) { 
+export async function AuthenticateUser(req:any, res:any, next:any) { 
     let token = req.header("user_token");
 
     if (!token) return res.status(401).json({ message: "Authentification impossible" });
     try {
         let decoded:any = jwt.verify(token, JWT_SECRET_KEY!);
         req.user = decoded.user;
-        next();
+        let userMap = await User.findById(req.user.id);
+        if(userMap === null)
+            res.status(401).json({ message: "Authentification impossible" });
+        else
+            next();
     } catch (e) {
+        console.log(e)
         res.status(401).json({ message: "Authentification impossible" });
     }
 }
